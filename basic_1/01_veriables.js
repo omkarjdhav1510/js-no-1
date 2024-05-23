2513c081709e88c491d2bcc393b7db8bb816bfad
omkarjdhav1510 <83813895+omkarjdhav1510@users.noreply.github.com>
2024-05-23 14:18:16 +0000

    data type

---
+++ basic_1/01_veriables.js
@@ -0,0 +1,17 @@
+const address = 123
+let post = "mumbai"
+var name = "mh"
+// address = ratnagiri  // is not allowed
+let sirname
+sirname="jadhav"
+let demo
+post="ddd"
+name = "jhgjhgjh"
+console.log(name);
+console.table([address,post,name,sirname,demo])
+
+/*
+not use to var
+because issue in block scope and function scope 
+*/
+
---
+++ basic_1/02_datatype.js
@@ -0,0 +1,20 @@
+let name = "omkar" //string
+let age = 25  //number
+let status = true //boolean
+let data; //undefine
+let marks = null  //null is standalone value (null type of object)
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+/*
+data types
+string => ""
+bigint
+number => 2 power 53
+boolean =>true/false
+null => standalone value
+undefine
+symbol =>unique
+*/
+
